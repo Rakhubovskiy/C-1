e698d22345d2604ed93d55605885cd13f18a788c
Ilia <rakhubovskiy@gmail.com>
2023-01-28 23:24:09 +0300

    add task0

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Clear();
+Console.Write("Insert the number: ");
+int number = int.Parse(Console.ReadLine()); // Переводим символный тип данных в числовой, с помощью int.Parse
+int sqr = number * number;
+Console.WriteLine("The square of the number " + number + " is " + sqr);
