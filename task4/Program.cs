0f60d27716c2db915c020c0526b47c8e50cf970f
Ilia <rakhubovskiy@gmail.com>
2023-01-29 20:04:42 +0300

    task4 completed

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,3 +2,9 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
+
+Console.Clear();
+Console.Write("Enter a three digit number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("The last digit in the number: ");
+Console.WriteLine(number % 10);
