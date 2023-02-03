b0367cd5a34d93416813c0ffbe48da41f001bc7d
Ilia <rakhubovskiy@gmail.com>
2023-02-03 13:41:33 +0300

    add task12

---
+++ task12/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер плоскости: ");
+int plane = int.Parse(Console.ReadLine());
+
+if (plane > 4 || plane < 0)
+{
+    Console.WriteLine("Такой плоскости нет");
+}
+else
+{
+    if (plane == 1)
+    {
+        Console.WriteLine("Координаты X > 0 , Y > 0");
+    }
+    else if (plane == 2)
+    {
+        Console.WriteLine("Координаты X < 0 , Y > 0");
+    }
+    else if (plane == 3)
+    {
+        Console.WriteLine("Координаты X < 0 , Y < 0");
+    }
+    else if (plane == 4)
+    {
+        Console.WriteLine("Координаты X > 0 , Y < 0");
+    }
+}
