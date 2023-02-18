389ad5a600654ec4b7c24e2d328005ad130bd7f9
Ilia <rakhubovskiy@gmail.com>
2023-02-18 18:08:11 +0300

    add task26

---
+++ task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+Console.Write("Введите десятичное число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write($"Двоичное число: {DecimalToBinar(num)}");
+
+
+string DecimalToBinar(int someNum)
+{
+    string str = "";
+    do
+    {
+        str = Convert.ToString(someNum % 2) + str;
+        someNum = someNum / 2;
+    }
+    while (someNum != 0);
+    return str;
+}
