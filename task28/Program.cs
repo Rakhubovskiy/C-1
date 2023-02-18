9942e1f8022c905a1af8c4b18f3c6011237cf1b8
Ilia <rakhubovskiy@gmail.com>
2023-02-18 18:08:33 +0300

    add task28

---
+++ task28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+Console.Clear();
+int[] array = GetArray(10, 0, 9);
+Console.WriteLine(string.Join(" ", array));
+int[] cloneArray = CloneArray(array);
+Console.WriteLine(string.Join(" ", cloneArray));
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] res = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        res[i] = new Random().Next(min, max + 1);
+    }
+    return res;
+}
+
+int[] CloneArray(int[] someArray)
+{
+    int length = someArray.Length;
+    int[] res = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        res[i] = someArray[i];
+    }
+    return res;
+}
