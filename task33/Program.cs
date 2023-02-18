a877ff168f01a3df7e3f2858f72fee0a8b66c6a7
Ilia <rakhubovskiy@gmail.com>
2023-02-18 21:43:08 +0300

    add task33

---
+++ task33/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив размера m на n.
+// Найдите все нечётные элементы, стоящиее на чётных местах.
+// Сложите эти элементы.
+
+Console.Clear();
+
+Console.Write("Введите количество строк  массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцы  массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение элемента: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов главной диагонали: {SumNum(array)}");
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumNum(int[,] inArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if ((i % 2 == 0) && (j % 2 == 0))
+            {
+                if (inArray[i, j] % 2 != 0)
+                {
+                    sum += inArray[i, j];
+                }
+            }
+        }
+    }
+    return sum;
+}
